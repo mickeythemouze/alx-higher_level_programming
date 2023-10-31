de5b203742ae56f5b13cbe36083faabd6abadbce
mickeythemouze <d.fikayo@yahoo.com>
2023-10-31 13:37:56 -0700

    very tired

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    """Match bytecode provided by ALX"""
+    if a < b:
+        return (c)
+    if C > b:
+        return (a + b)
+    return (a*b - c)
