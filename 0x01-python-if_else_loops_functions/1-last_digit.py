801b8e87e9aa73cb1c7fcc560c470e803a3c04b9
mickeythemouze <d.fikayo@yahoo.com>
2023-10-31 07:12:51 -0700

    never to late to start an if party

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 i = abs(number) % 10
 if number < 0:
     i = -i
-print(f"Last digit of {number:d} is {i:d}", end="")
+print(f"Last digit of {number:d} is {i:d} ", end="")
 if i > 5:
     print('and is greater than 5')
 elif i == 0:
